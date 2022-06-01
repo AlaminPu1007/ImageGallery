4ecce4ad7d21c8df0899047ce0a8abb35dfcf034
AlaminPu1007 <alamin66.sit@gmail.com>
2022-06-01 19:25:25 +0600

    removed redux from our projects

--- ImageGallery/App.tsx
+++ ImageGallery/App.tsx
@@ -1,21 +1,11 @@
 /**
  * This is the initial page of our application
- * we wrap our whole component with redux
- * to manage data globally
  */
 import React from 'react';
 import NavigationFlow from './src/NavigationFlow';
-import {Provider} from 'react-redux';
-import store from './src/redux/store';
 
 const App = () => {
   return <NavigationFlow />;
 };
 
-export default () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
+export default App;
